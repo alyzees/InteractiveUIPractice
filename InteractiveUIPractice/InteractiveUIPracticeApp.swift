349bc89d33f1afc476aae97c95a44ef410006626
Alyzee Sosa <alyzeesosacollege@gmail.com>
2023-07-13 13:15:47 -0700

    Initial Commit

---
+++ InteractiveUIPractice/InteractiveUIPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  InteractiveUIPracticeApp.swift
+//  InteractiveUIPractice
+//
+//  Created by Scholar on 7/13/23.
+//
+
+import SwiftUI
+
+@main
+struct InteractiveUIPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
